50a4c49e674812554fe1a92ece4858a3d7169434
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-08-10 02:41:10 +0300

    Corrected prettify parser and added escape hatch

--- src/prettify.rs
+++ src/prettify.rs
@@ -2,57 +2,48 @@ use anyhow::{anyhow, Result};
 use colored::{ColoredString, Colorize};
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_till, take_while1},
+    bytes::complete::{tag, take_until, take_while1},
     combinator::{eof, map},
     multi::many_till,
     sequence::delimited,
     IResult,
 };
-use std::fmt::Write;
 
 fn parse_bold(i: &str) -> IResult<&str, ColoredString> {
-    let second = take_till(|c| c == '<');
-    let marks = delimited(tag("<b>"), second, tag("</b>"));
+    let body = take_until("</b>");
+    let marks = delimited(tag("<b>"), body, tag("</b>"));
     map(marks, |c: &str| c.bold())(i)
 }
 
 fn parse_italic(i: &str) -> IResult<&str, ColoredString> {
-    let second = take_till(|c| c == '<');
-    let marks = delimited(tag("<i>"), second, tag("</i>"));
+    let body = take_until("</i>");
+    let marks = delimited(tag("<i>"), body, tag("</i>"));
     map(marks, |c: &str| c.italic())(i)
 }
 
 fn parse_plain(i: &str) -> IResult<&str, ColoredString> {
-    let before_marker = take_till(|c| c == '<');
-    map(before_marker, |c: &str| c.clear())(i)
-}
-
-fn parse_plain2(i: &str) -> IResult<&str, ColoredString> {
-    let eol = take_while1(|_| true);
-    map(eol, |c: &str| c.clear())(i)
-}
-
-fn parse_line(i: &str) -> IResult<&str, Vec<ColoredString>> {
-    let fst = alt((parse_bold, parse_italic, parse_plain, parse_plain2));
-    let (a, (b, _)) = many_till(fst, eof)(i)?;
-    Ok((a, b))
+    let body = alt((take_until("<b>"), take_until("<i>"), take_while1(|_| true)));
+    map(body, |c: &str| c.clear())(i)
 }
 
 fn prettify_inner(input: &str) -> Result<String> {
-    // band aid for Eternal Summoner.
-    let input = input.replace("<b><b>", "<b>").replace("</b>.</b>", ".</b>"); 
+    let apply_parsers = alt((parse_bold, parse_italic, parse_plain));
+    let (_, (parsed, _)) =
+        many_till(apply_parsers, eof)(&input).map_err(|e| anyhow!(e.to_string()))?;
 
-    let mut buffer = String::new();
+    let ret = parsed
+        .into_iter()
+        .fold(String::new(), |acc, s| format!("{acc}{s}"));
 
-    let (_, parsed) = parse_line(&input).map_err(|e| anyhow!(format!("{e}")))?;
-
-    for part in parsed {
-        write!(buffer, "{part}")?;
-    }
-
-    Ok(buffer)
+    Ok(ret)
 }
 
 pub fn prettify(input: &str) -> String {
-    prettify_inner(input).expect("prettify_error")
+    // band aid for Eternal Summoner:   <b><b>Reborn</b>.</b> <b>Deathrattle:</b> Summon 1 Eternal Knight.
+    let input = input.replace("<b><b>", "<b>").replace("</b>.</b>", ".</b>");
+
+    match prettify_inner(&input) {
+        Ok(ret) => ret,
+        Err(_) => input.to_owned(),
+    }
 }

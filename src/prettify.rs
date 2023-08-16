dd687e750bd2c075ec8299b0e2b47a6e8936af8f
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-08-16 10:01:07 +0300

    code cleanup

--- src/prettify.rs
+++ src/prettify.rs
@@ -2,27 +2,32 @@ use anyhow::{anyhow, Result};
 use colored::Colorize;
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_until, take_while1},
-    combinator::{eof, map},
+    bytes::complete::{tag, take_until},
+    combinator::{eof, map, rest},
     multi::many_till,
     sequence::delimited,
     IResult,
 };
 
+const B_BEG: &str = "<b>";
+const B_END: &str = "</b>";
+const I_BEG: &str = "<i>";
+const I_END: &str = "</i>";
+
 fn parse_bold(i: &str) -> IResult<&str, String> {
-    let body = take_until("</b>");
-    let marks = delimited(tag("<b>"), body, tag("</b>"));
+    let body = take_until(B_END);
+    let marks = delimited(tag(B_BEG), body, tag(B_END));
     map(marks, |c: &str| c.bold().to_string())(i)
 }
 
 fn parse_italic(i: &str) -> IResult<&str, String> {
-    let body = take_until("</i>");
-    let marks = delimited(tag("<i>"), body, tag("</i>"));
+    let body = take_until(I_END);
+    let marks = delimited(tag(I_BEG), body, tag(I_END));
     map(marks, |c: &str| c.italic().to_string())(i)
 }
 
 fn parse_plain(i: &str) -> IResult<&str, String> {
-    let body = alt((take_until("<b>"), take_until("<i>"), take_while1(|_| true)));
+    let body = alt((take_until(B_BEG), take_until(I_BEG), rest));
     map(body, |c: &str| c.to_owned())(i)
 }
 
@@ -36,10 +41,10 @@ fn prettify_inner(input: &str) -> Result<String> {
     Ok(ret)
 }
 
-pub fn prettify(input: &str) -> String {
+pub(crate) fn prettify(input: &str) -> String {
     let mut pass = input.to_owned();
 
-    while pass.contains("<b>") || pass.contains("<i>") {
+    while pass.contains(B_BEG) || pass.contains(I_BEG) {
         match prettify_inner(&pass) {
             Ok(s) => pass = s,
             Err(_) => break,

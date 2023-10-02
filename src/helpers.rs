33c740d4d5d5a8fadc47e052ec8332a83fceceae
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-10-02 16:57:56 +0300

    turns out i dont need simplify or nested_bold

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,3 @@
-#![allow(unused)]
-
-use std::fmt::Display;
-
 use colored::Colorize;
 use nom::{
     branch::alt,
@@ -11,6 +7,7 @@ use nom::{
     sequence::delimited,
     IResult,
 };
+use std::fmt::Display;
 
 // ====================
 // Text Tree
@@ -24,95 +21,8 @@ enum TextTree {
     Seq(Vec<TextTree>),
 }
 
-impl TextTree {
-    fn in_bold(self) -> Self {
-        Self::Bold(Box::new(self))
-    }
-
-    fn in_italic(self) -> Self {
-        Self::Italic(Box::new(self))
-    }
-
-    fn from_string(input: &str) -> Self {
-        Self::String(input.to_owned())
-    }
-
-    // Probably not the best way to tackle the problem
-    // feels too much.
-    fn simplify(self) -> Self {
-        let inner_fn = |input| match input {
-            Self::Bold(inner) => match *inner {
-                Self::Bold(_) => inner.simplify(),
-                _ => inner.simplify().in_bold(),
-            },
-            Self::Italic(inner) => match *inner {
-                Self::Italic(_) => inner.simplify(),
-                Self::Bold(inner_inner) => inner_inner.simplify().in_italic().in_bold(),
-                _ => inner.simplify().in_italic(),
-            },
-            _ => input,
-        };
-
-        let mut simplified = inner_fn(self);
-        let mut take_two = inner_fn(simplified.clone());
-
-        while take_two != simplified {
-            simplified = take_two;
-            take_two = inner_fn(simplified.clone());
-        }
-
-        simplified
-    }
-}
-
-#[cfg(test)]
-mod text_tree_tests {
-    use super::*;
-    use TextTree as TT;
-
-    #[test]
-    fn test_simplify_nested_bolds() {
-        let case = TT::from_string("5").in_bold().in_bold();
-        let case = case.simplify();
-        let expected = TT::from_string("5").in_bold();
-
-        assert_eq!(case, expected);
-    }
-
-    #[test]
-    fn test_simplify_nested_italics() {
-        let case = TT::from_string("5").in_italic().in_italic();
-        let case = case.simplify();
-        let expected = TT::from_string("5").in_italic();
-
-        assert_eq!(case, expected);
-    }
-
-    #[test]
-    fn test_simplify_nested_bold_italic_bold() {
-        let case = TT::from_string("5").in_bold().in_italic().in_bold();
-        let case = case.simplify();
-        let expected = TT::from_string("5").in_italic().in_bold();
-
-        assert_eq!(case, expected);
-    }
-
-    #[test]
-    fn test_simplify_nested_bold_italic_bold_italic() {
-        let case = TT::from_string("5")
-            .in_italic()
-            .in_bold()
-            .in_italic()
-            .in_bold();
-        let case = case.simplify();
-        let expected = TT::from_string("5").in_italic().in_bold();
-
-        assert_eq!(case, expected);
-    }
-}
-
 // ====================
-// Card Text on Console
+// Card Text to Console
 // ====================
 
 impl Display for TextTree {
@@ -153,68 +63,195 @@ fn parse_body(i: &str) -> IResult<&str, TextTree> {
     })(i)
 }
 
+fn to_text_tree(i: &str) -> Result<TextTree, &str> {
+    all_consuming(parse_body)(dbg!(i))
+        .map(|(_, s)| dbg!(s))
+        .map_err(|_| i)
+}
+
 pub(crate) fn prettify(i: &str) -> String {
-    all_consuming(parse_body)(i)
-        .map(|(_, s)| s.to_string())
+    to_text_tree(i)
+        .map(|s| s.to_string())
         .unwrap_or(i.to_owned())
 }
 
+#[cfg(test)]
+mod prettify_tests {
+    use super::*;
+    use TextTree as TT;
+
+    impl TextTree {
+        fn in_bold(input: Self) -> Self {
+            Self::Bold(Box::new(input))
+        }
+
+        fn in_italic(input: Self) -> Self {
+            Self::Italic(Box::new(input))
+        }
+
+        fn from_string(input: &str) -> Self {
+            Self::String(input.to_owned())
+        }
+    }
+
+    #[test]
+    fn test_climactic_necrotic_explosion() -> Result<(), String> {
+        let input = "<b>Lifesteal</b>. Deal damage. Summon / Souls. <i>(Randomly improved by <b>Corpses</b> you've spent)</i>";
+        let case = to_text_tree(dbg!(input))?;
+        let expected = TT::Seq(vec![
+            TT::in_bold(TT::from_string("Lifesteal")),
+            TT::from_string(". Deal damage. Summon / Souls. "),
+            TT::in_italic(TT::Seq(vec![
+                TT::from_string("(Randomly improved by "),
+                TT::in_bold(TT::from_string("Corpses")),
+                TT::from_string(" you've spent)"),
+            ])),
+        ]);
+
+        assert_eq!(dbg!(case), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_eternal_summoner() -> Result<(), String> {
+        let input = "<b><b>Reborn</b>.</b> <b>Deathrattle:</b> Summon 1 Eternal Knight.";
+        let case = to_text_tree(dbg!(input))?;
+        let expected = TT::Seq(vec![
+            TT::in_bold(TT::Seq(vec![
+                TT::in_bold(TT::from_string("Reborn")),
+                TT::from_string("."),
+            ])),
+            TT::from_string(" "),
+            TT::in_bold(TT::from_string("Deathrattle:")),
+            TT::from_string(" Summon 1 Eternal Knight."),
+        ]);
+
+        assert_eq!(dbg!(case), expected);
+        Ok(())
+    }
+}
 // ====================
 // Card Text on Image
 // ====================
 
-enum TextStyle {
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct TextPiece {
+    text: String,
+    style: TextStyle,
+}
+
+impl TextPiece {
+    fn new(text: &str, style: TextStyle) -> Self {
+        TextPiece {
+            text: text.into(),
+            style,
+        }
+    }
+
+    fn embolden(self) -> Self {
+        Self {
+            style: match self.style {
+                TextStyle::Plain => TextStyle::Bold,
+                TextStyle::Italic => TextStyle::BoldItalic,
+                _ => self.style,
+            },
+            ..self
+        }
+    }
+
+    fn italicize(self) -> Self {
+        Self {
+            style: match self.style {
+                TextStyle::Plain => TextStyle::Italic,
+                TextStyle::Bold => TextStyle::BoldItalic,
+                _ => self.style,
+            },
+            ..self
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum TextStyle {
     Plain,
     Bold,
     Italic,
     BoldItalic,
-
-    // necessary for some odd cases of nested bolds. either this or simplify
-    Black,
-    BlackItalic,
 }
 
-impl TextStyle {
-    fn bold(self) -> Self {
-        match self {
-            Self::Plain => Self::Bold,
-            Self::Bold => Self::Black,
-            Self::Italic => Self::BoldItalic,
-            Self::BoldItalic => Self::BlackItalic,
-            _ => self,
-        }
+/* // ==== Visitor Pattern. Works but reaches "recursion limit".
+fn traverse_inner(tree: TextTree, mut visit: impl FnMut(TextPiece)) {
+    match tree {
+        TextTree::String(text) => visit(TextPiece::new(&text, TextStyle::Plain)),
+        TextTree::Bold(inner) => traverse_inner(*inner, |tp| visit(tp.embolden())),
+        TextTree::Italic(inner) => traverse_inner(*inner, |tp| visit(tp.italicize())),
+        TextTree::Seq(seq) => seq
+            .into_iter()
+            .for_each(|tt| traverse_inner(tt, |tp| visit(tp))),
+    }
+} */
+
+#[allow(unused)]
+fn traverse_text_tree(tree: TextTree) -> Vec<TextPiece> {
+    let mut collector = vec![];
+
+    match tree {
+        TextTree::String(text) => collector.push(TextPiece::new(&text, TextStyle::Plain)),
+        TextTree::Bold(inner) => traverse_text_tree(*inner)
+            .into_iter()
+            .for_each(|piece| collector.push(piece.embolden())),
+        TextTree::Italic(inner) => traverse_text_tree(*inner)
+            .into_iter()
+            .for_each(|piece| collector.push(piece.italicize())),
+        TextTree::Seq(seq) => seq
+            .into_iter()
+            .flat_map(traverse_text_tree)
+            .for_each(|piece| collector.push(piece)),
     }
 
-    fn unbold(self) -> Self {
-        match self {
-            Self::Bold => Self::Plain,
-            Self::Black => Self::Bold,
-            Self::BoldItalic => Self::Italic,
-            Self::BlackItalic => Self::BoldItalic,
-            _ => self,
-        }
-    }
-
-    fn italic(self) -> Self {
-        match self {
-            Self::Plain => Self::Italic,
-            Self::Bold => Self::BoldItalic,
-            Self::Black => Self::BlackItalic,
-            _ => self,
-        }
-    }
-
-    fn unitalic(self) -> Self {
-        match self {
-            Self::Italic => Self::Plain,
-            Self::BoldItalic => Self::Bold,
-            Self::BlackItalic => Self::Black,
-            _ => self,
-        }
-    }
+    collector
 }
 
-struct TextPiece {
-    text: String,
-    style: TextStyle,
+#[cfg(test)]
+mod traverse_tests {
+    use super::*;
+    use TextPiece as TP;
+    use TextStyle as TS;
+
+    #[test]
+    fn test_eternal_summoner() -> Result<(), String> {
+        let input = "<b><b>Reborn</b>.</b> <b>Deathrattle:</b> Summon 1 Eternal Knight.";
+        let tree = to_text_tree(dbg!(input))?;
+
+        let traversal = traverse_text_tree(tree);
+
+        let expected = vec![
+            TP::new("Reborn", TS::Bold),
+            TP::new(".", TS::Bold),
+            TP::new(" ", TS::Plain),
+            TP::new("Deathrattle:", TS::Bold),
+            TP::new(" Summon 1 Eternal Knight.", TS::Plain),
+        ];
+
+        assert_eq!(dbg!(traversal), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_climactic_necrotic_explosion() -> Result<(), String> {
+        let input = "<b>Lifesteal</b>. Deal damage. Summon / Souls. <i>(Randomly improved by <b>Corpses</b> you've spent)</i>";
+        let tree = to_text_tree(dbg!(input))?;
+        let traversal = traverse_text_tree(tree);
+
+        let expected = vec![
+            TP::new("Lifesteal", TS::Bold),
+            TP::new(". Deal damage. Summon / Souls. ", TS::Plain),
+            TP::new("(Randomly improved by ", TS::Italic),
+            TP::new("Corpses", TS::BoldItalic),
+            TP::new(" you've spent)", TS::Italic),
+        ];
+
+        assert_eq!(dbg!(traversal), expected);
+        Ok(())
+    }
 }

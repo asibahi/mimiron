e43e691da6c3d4f331d9008ec0a22d41aa5a88d5
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-12-31 23:12:36 +0300

    bugfix :o

--- mimiron/src/bg.rs
+++ mimiron/src/bg.rs
@@ -337,7 +337,7 @@ pub fn lookup(opts: &SearchOptions) -> Result<impl Iterator<Item = Card> + '_> {
     if let Some(t) = &opts.minion_type {
         res = res.query(
             "minionType",
-            &t.in_locale(Locale::enUS).to_string(), // Is it always enUS?
+            &t.in_locale(Locale::enUS).to_string().to_lowercase(), // Is it always enUS?
         );
     }
 

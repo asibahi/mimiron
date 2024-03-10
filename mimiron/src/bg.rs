b8f84a2e42b7ce906078c9bbcdfe383ba506de07
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-03-10 21:42:49 +0300

    fix: filter out buddies and bananas from hero powers.

--- mimiron/src/bg.rs
+++ mimiron/src/bg.rs
@@ -367,8 +367,13 @@ pub fn get_and_print_associated_cards(card: &Card, locale: Locale) -> Vec<Card> 
                 // versions of hero powers below that for some reason.
                 // First hero power is usually the smallest ID.
                 //
-                // Card ID 70136 is Blood Gem.
-                let Some(id) = child_ids.iter().filter(|&&id| id != 70136).min() else {
+                // Card ID 70136 is Blood Gem. 1694 is Banana
+                // Workaround for Blackthorn and Mukla, respectively.
+                let Some(id) = child_ids
+                    .iter()
+                    .filter(|&&id| id != 70136 && id != 1694 && &Some(id) != buddy_id)
+                    .min()
+                else {
                     break 'heropower;
                 };
                 let Ok(res) = get_card_by_id(*id, locale) else {

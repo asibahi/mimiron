ea8c4d6c6b31df18b24514f62f58cf1fc24b511b
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-04-03 18:40:57 +0300

    clippy, name corrections, and waffling

--- mimiron/src/deck.rs
+++ mimiron/src/deck.rs
@@ -372,40 +372,32 @@ fn raw_data_to_deck(opts: &LookupOptions, raw_data: RawCodeData, title: Option<S
     deck
 }
 
-fn specific_card_adjustments(deck: &mut Deck) -> () {
+fn specific_card_adjustments(deck: &mut Deck) {
     // This function contains specific adjustments to specific cards as needed.
 
-    // Treatments for Zilliax 3000 Deluxe
-    '_zilliax_3000_deluxe: {
-        const ZILLIAX_3000_DELUXE_ID: usize = 102983;
+    // Treatments for Zilliax Deluxe 3000
+    const ZILLIAX_DELUXE_3000_ID: usize = 102_983;
+    '_zilliax_deluxe_3000: for sb in deck.sideboard_cards.iter_mut().flatten() {
+        // removes cosmetic cards from all sideboards.
+        // Currently only has an effect on Zilliax Cosmetic Modules
+        sb.cards_in_sideboard.retain(|c| !c.cosmetic);
 
-        for sb in deck.sideboard_cards.iter_mut().flatten() {
-            // removes cosmetic cards from all sideboards.
-            // Currently only has an effect on Zilliax Cosmetic Modules
-            sb.cards_in_sideboard.retain(|c| !c.cosmetic);
-
-            if sb.sideboard_card.id == ZILLIAX_3000_DELUXE_ID {
-                let (zilliax_cost, zilliax_attack, zilliax_health) =
-                    sb.cards_in_sideboard.iter().fold((0, 0, 0), |(acc_c, acc_a, acc_h), c| {
-                        let (a, h) = c.stats();
-                        (
-                            acc_c + c.cost,
-                            acc_a + a.unwrap_or_default(),
-                            acc_h + h.unwrap_or_default(),
-                        )
-                    });
-                deck.cards.iter_mut().find(|c| c.id == ZILLIAX_3000_DELUXE_ID).map(|c| {
-                    let CardType::Minion { ref mut attack, ref mut health, .. } = c.card_type
-                    else {
-                        unreachable!()
-                    };
-                    c.cost = zilliax_cost;
-                    *attack = zilliax_attack;
-                    *health = zilliax_health;
-
-                    c
+        if sb.sideboard_card.id == ZILLIAX_DELUXE_3000_ID {
+            let (zilliax_cost, zilliax_attack, zilliax_health) =
+                sb.cards_in_sideboard.iter().fold((0, 0, 0), |(acc_c, acc_a, acc_h), c| {
+                    let (a, h) = c.stats();
+                    (acc_c + c.cost, acc_a + a.unwrap_or_default(), acc_h + h.unwrap_or_default())
                 });
-            }
+            deck.cards.iter_mut().find(|c| c.id == ZILLIAX_DELUXE_3000_ID).map(|c| {
+                let CardType::Minion { ref mut attack, ref mut health, .. } = c.card_type else {
+                    unreachable!("Zilliax Deluxe 3000 is a minion.")
+                };
+                c.cost = zilliax_cost;
+                *attack = zilliax_attack;
+                *health = zilliax_health;
+
+                c
+            });
         }
     }
 }

276045f53a6350d84bf35c4a865187148064103a
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-11-11 03:19:46 +0300

    clean up dead code and general waffling

--- mimiron/src/deck.rs
+++ mimiron/src/deck.rs
@@ -2,7 +2,6 @@ use crate::{
     card::{self, Card},
     card_details::{CardType, Class, Details},
     get_access_token,
-    hearth_sim::validate_id,
     localization::{Locale, Localize},
     AGENT,
 };
@@ -13,7 +12,6 @@ use base64::{
 };
 use colored::Colorize;
 use compact_str::{format_compact, CompactString, ToCompactString};
-use counter::Counter;
 use itertools::Itertools;
 use serde::Deserialize;
 use std::{
@@ -59,9 +57,9 @@ impl From<String> for Format {
         }
     }
 }
-impl TryFrom<u8> for Format {
+impl TryFrom<usize> for Format {
     type Error = anyhow::Error;
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
         Ok(match value {
             1 => Self::Wild,
             2 => Self::Standard,
@@ -105,6 +103,8 @@ pub struct Deck {
 impl Deck {
     #[must_use]
     pub fn compare_with(&self, other: &Self) -> DeckDifference {
+        use counter::Counter;
+
         let counter1 = self.cards.clone().into_iter().collect::<Counter<_>>();
         let counter2 = other.cards.clone().into_iter().collect::<Counter<_>>();
 
@@ -239,87 +239,85 @@ impl<'s> LookupOptions<'s> {
 #[derive(Default)]
 struct RawCodeData {
     format: Format,
-    hero: usize,
     cards: Vec<usize>,
     sideboard_cards: Vec<(usize, usize)>,
     deck_code: CompactString,
 }
 
-const CONFIG: GeneralPurposeConfig = GeneralPurposeConfig::new().with_decode_padding_mode(DecodePaddingMode::Indifferent);
+const CONFIG: GeneralPurposeConfig =
+    GeneralPurposeConfig::new().with_decode_padding_mode(DecodePaddingMode::Indifferent);
 const ENGINE: GeneralPurpose = GeneralPurpose::new(&alphabet::STANDARD, CONFIG);
 
 impl RawCodeData {
     fn parse(input: &[u8]) -> nom::IResult<&[u8], Self> {
         // Deckstring encoding: https://hearthsim.info/docs/deckstrings/
 
-        use nom::{combinator::map, multi::length_count, sequence::pair};
-
-        fn parse_varint(input: &[u8]) -> nom::IResult<&[u8], usize> {
-            use nom::{
-                bytes::complete::{take, take_till},
-                sequence::pair,
-            };
-        
-            let (rem, (p, lb)) = pair(take_till(|b: u8| b & 128 == 0), take(1u8))(input)?;
-            let n = p
-                .iter()
-                .chain(lb)
-                .enumerate()
-                .fold(0, |acc, (idx, byte)| acc | ((*byte as usize & 127) << (idx * 7)));
-        
-            Ok((rem, n))
-        }
-        
-        fn parse_card_id(input: &[u8]) -> nom::IResult<&[u8], usize> {
-            nom::combinator::map(parse_varint, validate_id)(input)
-        }
-
-        let mut decoded = input;
-        while let Ok((remainder, n)) = parse_varint(decoded) {
-            decoded = remainder;
-            println!("{n}");
-        }
-
-        #[allow(clippy::cast_possible_truncation)]
-         // starting from 2 as Format is the third number.
-        let (rem, format) =
-            map(parse_varint, |f| (f as u8).try_into().unwrap_or_default())(&input[2..])?;
-
-        // Hero ID is the 4th position
-        let (rem, hero) = parse_varint(&rem[1..])?;
-
-        let (rem, mut cards) = length_count(parse_varint, parse_card_id)(rem)?;
-
-         // double cards
-        let (rem, _) = length_count(
-            parse_varint,
-            map(parse_card_id, |id| {
-                let id = validate_id(id);
-                cards.push(id);
-                cards.push(id);
-            }),
-        )(rem)?;
-
-        // n-count cards
-        let (rem, _) = length_count(
-            parse_varint,
-            map(pair(parse_card_id, parse_varint), |(id, count)| {
-                for _ in 0..count {
-                    cards.push(id);
-                }
-            }),
-        )(rem)?;
-
-        let (rem, cond) = parse_varint(rem)?;
-        let (rem, sideboard_cards) = if cond == 1 {
-            length_count(parse_varint, pair(parse_card_id, parse_card_id))(rem)?
-        } else {
-            (rem, Vec::new())
+        use nom::{
+            bytes::complete::{take, take_till},
+            combinator::{cond, map, map_opt, map_res, opt, verify},
+            multi::{count, length_count},
+            sequence::{pair, tuple},
+            IResult,
         };
 
-        let result = RawCodeData {
+        #[allow(clippy::cast_possible_truncation)]
+        fn parse_varint(input: &[u8]) -> IResult<&[u8], usize> {
+            let is_last = |b| b & 0x80 == 0;
+            let is_in_bounds = |p: &[u8]| p.len() < 9;
+            let try_varint = |(p, lb): (&[u8], &[u8])|
+                p.iter().chain(lb).enumerate().try_fold(0, |acc, (idx, byte)|
+                    ((*byte as usize) & 0x7F).checked_shl(idx as u32 * 7).map(|n| acc | n)
+                );
+
+            map_opt(pair(verify(take_till(is_last), is_in_bounds), take(1u8)), try_varint)(input)
+        }
+
+        fn parse_card_id(input: &[u8]) -> IResult<&[u8], usize> {
+            map(parse_varint, crate::hearth_sim::validate_id)(input)
+        }
+
+        // let mut decoded = input;
+        // while let Ok((remainder, n)) = parse_varint(decoded) {
+        //     decoded = remainder;
+        //     println!("{n}");
+        // }
+
+        // starting from 2 as Format is the third number.
+        let (rem, format) = map_res(parse_varint, TryInto::try_into)(&input[2..])?;
+
+        // skip two numbers over Hero ID.
+        let (rem, _) = count(parse_varint, 2)(rem)?;
+        let (rem, cards) = map(
+            tuple((
+                // single cards
+                length_count(parse_varint, parse_card_id),
+
+                // double cards
+                length_count(parse_varint, map(parse_card_id, |id| [id; 2])),
+
+                // n-count cards
+                length_count(
+                    parse_varint,
+                    map(pair(parse_card_id, parse_varint), |(id, n)| [id].repeat(n)),
+                ),
+            )),
+            |(v1, v2, vn)| v1.into_iter()
+                    .chain(v2.into_iter().flatten())
+                    .chain(vn.into_iter().flatten())
+                    .collect(),
+        )(rem)?;
+
+        let (rem, c) = opt(parse_varint)(rem)?;
+        let (rem, sideboard_cards) = map(
+            cond(
+                c.is_some_and(|c| c == 1),
+                length_count(parse_varint, pair(parse_card_id, parse_card_id)),
+            ),
+            Option::unwrap_or_default,
+        )(rem)?;
+
+        let result = Self {
             format,
-            hero,
             cards,
             sideboard_cards,
             deck_code: ENGINE.encode(input).into(), // Hearthstone requires base64 padding
@@ -328,14 +326,12 @@ impl RawCodeData {
         Ok((rem, result))
     }
 
-    fn from_code (code :&str) -> Option<Self>{
+    fn from_code(code: &str) -> Option<Self> {
         let decoded = ENGINE.decode(code).ok()?;
 
-        RawCodeData::parse(&decoded).map(|(_,rd)| rd).ok()
+        Self::parse(&decoded).map(|(_, rd)| rd).ok()
     }
-
 }
-
 
 pub fn lookup(opts: LookupOptions<'_>) -> Result<Deck> {
     let code = &opts.code;
@@ -354,10 +350,9 @@ pub fn lookup(opts: LookupOptions<'_>) -> Result<Deck> {
         .find_map(RawCodeData::from_code)
 
         // if it is a url from the official deck builder
-        .or_else(||
-            code.split_terminator(&['=', '?'])
-                .find_map(|s| urlencoding::decode(s).as_deref().ok().and_then(RawCodeData::from_code))
-        )
+        .or_else(|| code.split_terminator(&['=', '?']).find_map(|s|
+            urlencoding::decode(s).as_deref().ok().and_then(RawCodeData::from_code)
+        ))
         .ok_or_else(|| anyhow!("Unable to parse deck code. Code may be invalid."))?;
 
     let title = code
